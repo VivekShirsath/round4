4a8016d574c4cdcc1f2124d2dc044bf5ab150101
Vivek Shirsath <vivekshirsath1310@gmail.com>
2023-06-24 22:18:50 +0530

    ui

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,9 @@
+import { useData } from "../DataContext"
 
 export const Sidebar = () => {
+    const {data} = useData();
     return(
-        <div className="w-1/4 flex flex-col p-4 gap-4 bg-gray-300 min-h-screen sticky top-0 self-start">
+        <div className="w-1/4 flex flex-col p-4 gap-4 bg-gray-300 min-h-screen sticky top-0 self-start relative">
             <div className="flex gap-3 p-4">
             <img src="https://cdn-icons-png.flaticon.com/128/1946/1946436.png"
             className="w-8"/>
@@ -18,6 +20,13 @@ export const Sidebar = () => {
             <div className="flex gap-3 p-4">
             <img src="https://cdn-icons-png.flaticon.com/128/456/456212.png" className="w-8"/>
             <h3>Profile</h3>
+            </div>
+            <div className="flex gap-3 absolute bottom-8">
+                <img src={data.picUrl} className="w-8"/>
+                <div className="flex flex-col">
+                    <h3>{data.name}</h3>
+                    <h3>@{data.username}</h3>
+                </div>
             </div>
         </div>
     )
